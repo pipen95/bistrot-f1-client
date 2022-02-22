bf4c5f835ed1eda03d5e4123d8304d1a55ab48b9
Pierre Penel <penel.pierre@gmail.com>
2022-02-22 08:14:47 -0500

    Login Validation JS

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,87 +1,114 @@
-import React, { useState, useRef } from 'react';
-import Form from 'react-validation/build/form';
-import Input from 'react-validation/build/input';
-import CheckButton from 'react-validation/build/button';
-import axios from 'axios';
+import React, {useState, useRef} from "react";
+import axios from "axios";
 
-
-const required = (value) => {
-  if (!value) {
-    return (
-      <div className="alert alert-danger" role="alert">
-        This field is required!
-      </div>
-    );
-  }
-};
-
-export const Login = ({ closeModal }) => {
-  const form = useRef();
-  const checkBtn = useRef();
+export const Login = ({closeModal }) => {
+  const email = useRef();
+  const password = useRef();
 
   const [submitting, setSubmitting] = useState(false);
-  const [message, setMessage] = useState('');
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
+    email: "",
+    password: ""
+  })
+  const [errors, setErrors] = useState({});
 
   console.log(formData);
 
   // HANDLER FONCTIONS
 
   const handleChange = (event) => {
-    return setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
+    return setFormData({...formData, [event.target.name]: event.target.value})
   };
+
+
+  const handleValidation = (data) => {
+    let fields = data;
+    let errors = {};
+    let formIsValid = true;
+
+    //Name
+    if (!fields["password"]) {
+      formIsValid = false;
+      errors["password"] = "Cannot be empty";
+    }
+
+    if (typeof fields["password"] !== "undefined") {
+      if (!fields["password"].match(/^[a-zA-Z]+$/)) {
+        formIsValid = false;
+        errors["password"] = "Only letters";
+      }
+    }
+
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof fields["email"] !== "undefined") {
+      let lastAtPos = fields["email"].lastIndexOf("@");
+      let lastDotPos = fields["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields["email"].indexOf("@@") == -1 &&
+          lastDotPos > 2 &&
+          fields["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+    }
+
+    this.setErrors({ errors: errors });
+    return formIsValid;
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessage('');
-    setSubmitting(true);
-
-    if (checkBtn.current.context._errors.length === 0) {
-      postData(formData, closeModal);
-      return setFormData({
-        email: '',
-        password: '',
-      });
+   
+    if (handleValidation(formData)) {
+      setSubmitting(true);
+      postData(formData,closeModal);
+      alert("Form submitted");
     } else {
-      setSubmitting(false);
+      alert("Form has errors.");
     }
+    return setFormData({
+      email: "",
+      password: ""
+    })
   };
 
-  // SUBMIT POST REQUEST
-  const postData = async (data, closeModal) => {
-    const payload = {
-      email: data.email,
-      password: data.password,
-    };
-    try {
-      const res = await axios.post(
-        'http://localhost:3001/api/v1/users/login',
-        payload
-      );
-      if (res) {
-        setSubmitting(false);
-        closeModal();
+
+
+
+
+
+    // SUBMIT POST REQUEST
+    const postData = async (data, closeModal) => {
+      const payload = {
+        email: data.email,
+        password: data.password, 
       }
-    } catch (error) {
-      console.log(error.name);
-      console.log(error.message);
-      console.log(error.stack);
-      const resMessage =
-        (error.response &&
-          error.response.data &&
-          error.response.data.message) ||
-        error.message ||
-        error.toString();
-      setSubmitting(false);
-      setMessage(resMessage);
-    }
-  };
+      try {
+        const res = await axios.post(
+          "http://localhost:3001/api/v1/users/login",
+          payload
+        );
+        if (res) {
+          setSubmitting(false);
+          closeModal();
+        }
+      } catch (error) {
+        console.log(error.name);
+        console.log(error.message);
+        console.log(error.stack);
+      }
+    };
 
   // JSX FORM
   return (
@@ -92,55 +119,44 @@ export const Login = ({ closeModal }) => {
         </>
       ) : (
         <>
-          <Form onSubmit={handleSubmit} ref={form}>
-            <h2 className="text-center">Login Form</h2>
-            <div>
-              <fieldset className="form-group" disabled={submitting}>
-                <label htmlFor="email">Email</label>
-                <Input
-                  type="text"
-                  id="email"
-                  className="form-control"
-                  name="email"
-                  onChange={handleChange}
-                  value={formData.email}
-                  validations={[required]}
-                />
-              </fieldset>
-              <fieldset className="form-group" disabled={submitting}>
-                <label htmlFor="password">Password</label>
-                <Input
-                  type="text"
-                  id="password"
-                  className="form-control"
-                  name="password"
-                  onChange={handleChange}
-                  value={formData.password}
-                  validations={[required]}
-                />
-              </fieldset>
-            </div>
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-center">
+              Login Form
+            </h2>
+              <div>
+                <fieldset className="form-group" disabled={submitting}>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="text"
+                    id="email"
+                    className="form-control"
+                    name="email"
+                    onChange={handleChange}
+                    value={formData.email}
+                  />
+                </fieldset>
+                <fieldset className="form-group" disabled={submitting}>
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="text"
+                    id="password"
+                    className="form-control"
+                    name="password"
+                    onChange={handleChange}
+                    value={formData.password}
+                  />
+                </fieldset>
+              </div>
             <div className="form-group">
               <button
                 className="form-control btn btn-primary"
                 type="submit"
                 disabled={submitting}
               >
-                {submitting && (
-                  <span className="spinner-border spinner-border-sm"></span>
-                )}
-                Login
+                Submit
               </button>
             </div>
-            {message && (
-            <div className="form-group">
-              <div className="alert alert-danger" role="alert">
-                {message}
-              </div>
-            </div>
-          )}
-          <CheckButton style={{ display: "none" }} ref={checkBtn} />
-          </Form>
+          </form>
         </>
       )}
     </div>
