e8ed9a9fad3e33f8814b42283dd7b591a70da7c0
Pierre Penel <penel.pierre@gmail.com>
2022-03-02 07:59:34 -0500

    auth: delete logout req

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,20 +1,17 @@
-import axios from 'axios';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
 const setCookie = (data) => {
-  cookies.set("user", data, { path: '/' });
-}
+  cookies.set('user', data, { path: '/' });
+};
 
 const logout = async () => {
-  const res = await axios.get("/api/users/logout");
-  if (res) {
-    cookies.set("user", null, { path: '/' , expires : new Date(Date.now() -10 * 1000)});
-    window.location.reload(false);
-  } else {
-    console.log("an error has occured !");
-  }
+  cookies.set('user', null, {
+    path: '/',
+    expires: new Date(Date.now() - 10 * 1000),
+  });
+  window.location.reload(false);
 };
 
 const getCurrentUser = () => {
